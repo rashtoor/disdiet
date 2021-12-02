dff4ad6e15be62e8571b43f2c666cf0210e69084
rashtoor <90686887+rashtoor@users.noreply.github.com>
2021-12-02 13:38:34 +0530

    Update phase2.py

--- phase2.py
+++ phase2.py
@@ -430,7 +430,7 @@ def network_analysis(dis):
     html = ''
     html = addContent(html, header(
         'Harmful Diets for '+dis, color='black'))
-    html = addContent(html, box(dr_harm[col].tolist()))
+    html = addContent(html, box(dr_harm[col].to_html()))
     html = addContent(html, header(
         'Helpful Diets for '+dis, color='black'))
     html = addContent(html, box(dr_help[col].to_html()))
