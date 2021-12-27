55ee49a6fce1e57b16a150eee575f7fa19164d25
rashtoor <90686887+rashtoor@users.noreply.github.com>
2021-12-27 14:48:06 +0530

    Update ex.py

--- templates/ex.py
+++ templates/ex.py
@@ -1,6 +1,7 @@
 {% extends 'random.html' %}
 
 {% block content %}
+    <h1>{% block title %} graph {% endblock %}</h1>
     <h1>Hello World!</h1>
     <h2>Welcome to FlaskApp!</h2>
     <h3>{{ utc_dt }}</h3>
