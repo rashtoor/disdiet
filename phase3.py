7c6304110f7b1204dcacbb73cf7587b49131a40a
rashtoor <90686887+rashtoor@users.noreply.github.com>
2021-12-29 23:36:40 +0530

    Update phase3.py

--- phase3.py
+++ phase3.py
@@ -438,7 +438,7 @@ def network_analysis(dis):
 
     # Formatting in html
     #q2="perfect"
-    file = io.open("rnn_index.html", "r", "utf-8")
+    file = io.open("rnn_index.html", "r", encoding='utf-8')
     q=file.read()
     html = '{% extends' + q + '%} {% block content %}'
     html = addContent(html, header(
