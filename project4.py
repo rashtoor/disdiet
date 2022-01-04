0b0e8d3b70579d86c022986905909bee760ed1df
rashtoor <90686887+rashtoor@users.noreply.github.com>
2022-01-04 14:21:12 +0530

    Update project4.py

--- project4.py
+++ project4.py
@@ -103,7 +103,7 @@ def get_graph():
                 i += 1
             rels.append({"source": source, "target": target})
    
-    return Response(dumps({"nodes": nodes, "links": rels}),mimetype="application/json")
+    return render_template('work.html', graph=Response(dumps({"nodes": nodes, "links": rels}),mimetype="application/json"))
 
 
 
