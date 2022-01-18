bc914742eb16960766c9e5af0cb595ca3a4e9dd8
rashtoor <90686887+rashtoor@users.noreply.github.com>
2022-01-18 17:33:30 +0530

    Update project4.py

--- project4.py
+++ project4.py
@@ -95,7 +95,8 @@ def g():
         print(seed2)
         # seed is the disease that person has selected, so now we read csv, create its graph and apply all network analysis and return significant diets using a function
         if seed2 != ' ':
-            return render_template('gg.html',input=na(dis=seed2))
+            #return render_template('gg.html',input=na(dis=seed2))
+            return render_template('solemn1.html')
         
             # Send template information to index.html
     return render_template('rnn_index.html', form=form2)
