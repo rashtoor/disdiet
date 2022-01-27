43bf11a6c7b7a9996223863fd4bb734f8f84af12
rashtoor <90686887+rashtoor@users.noreply.github.com>
2022-01-27 17:28:26 +0530

    Update phase3.py

--- phase3.py
+++ phase3.py
@@ -508,7 +508,7 @@ def network_analysis(dis):
 def header(text, color='black'):
     """Create an HTML header"""
 
-    raw_html = '<tr><td><div style="float: center;"><h1 style="margin-top:12px;color: {color};font-size:28px"><center>' + str(
+    raw_html = '<tr><td><div style="float: center;"><h1 style="margin-top:12px;color: {color};font-size:30px"><center>' + str(
             text) + '</center></h1></div></td></tr>'
     return raw_html
 
