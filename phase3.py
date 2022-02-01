4c4e72b78516a2cd158111f2f4aec89f331f6505
rashtoor <90686887+rashtoor@users.noreply.github.com>
2022-02-01 12:01:12 +0530

    Update phase3.py

--- phase3.py
+++ phase3.py
@@ -549,12 +549,12 @@ def box1(text):
 
 def box2(text):
     """Create an HTML box of text"""
-    raw_html = '<td width=100%><div style="border-bottom:1px inset black;border-top:1px inset black;padding:8px;font-size: 15px;float: left;"><table border="3" align="center"><tr><th colspan="4">Conclusion</th></tr><tr><td>Considering cut off percentage of 70%, the diets harmful for CD are::<br>' + text.to_string(index=False) + '</div></td></tr></table></td></tr></table>'
+    raw_html = '<td width=100%><div style="border-bottom:1px inset black;border-top:1px inset black;padding:8px;font-size: 15px;float: left;"><table border="3" align="center"><tr><th colspan="4">Conclusion</th></tr><tr><td>Considering cut off percentage of 70%, the diets harmful for CD are::<br>' + text.to_html(index=False) + '</div></td></tr></table></td></tr></table>'
     return raw_html
 
 def box3(text):
     """Create an HTML box of text"""
-    raw_html = '<td width=100%><div style="border-bottom:1px inset black;border-top:1px inset black;padding:8px;font-size: 15px;float: left;"><table border="3" align="center"><tr><th colspan="4">Conclusion</th></tr><tr><td>Considering cut off percentage of 70%, the diets helpful for CD are::<br>' + text.to_string(index=False) + '</div></td></tr></table></td></tr></table>'
+    raw_html = '<td width=100%><div style="border-bottom:1px inset black;border-top:1px inset black;padding:8px;font-size: 15px;float: left;"><table border="3" align="center"><tr><th colspan="4">Conclusion</th></tr><tr><td>Considering cut off percentage of 70%, the diets helpful for CD are::<br>' + text.to_html(index=False) + '</div></td></tr></table></td></tr></table>'
     return raw_html
 
 def bar(filename):
